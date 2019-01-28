fbaa66a86b33adbb26b93791f8ee318f59a4545f
chandana-bharathala <bharathalachandana@gmail.com>
2019-01-28 15:27:27 +0530

    18031j0023

---
+++ module 1/assignment1.py
@@ -0,0 +1,8 @@
+first=input("enter the number\n")
+#type(first)
+second = input("enter the number\n")
+#type(second)
+
+print(int(first)+int(second))
+print(int(first)-int(second))
+print(int(first)*int(second))
---
+++ module 1/assignment2p1.py
@@ -0,0 +1,11 @@
+#n= 32
+#n=12
+#n=21
+
+n=int(input("enter a number \n"))
+
+m=21
+if n<=m:
+	print(m-n)
+else:
+	print((n-m)*2)
